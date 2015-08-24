e4b024f85c3afef63317e6fdbd061688178b0cc8
Fabio3rs <fabio3rs@gmail.com>
2015-08-24 00:39:38 -0300

    Started CGamePlay

---
+++ Guitar++/CGamePlay.cpp
@@ -0,0 +1,35 @@
+#include "CGamePlay.h"
+
+
+
+void CGamePlay::update()
+{
+
+
+
+}
+
+
+
+void CGamePlay::render()
+{
+
+
+
+}
+
+/*
+CGamePlay &CGamePlay::gamePlay()
+{
+	static CGamePlay game;
+	return game;
+}*/
+
+CGamePlay::CGamePlay()
+{
+
+
+}
+
+
+
