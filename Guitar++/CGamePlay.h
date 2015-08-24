2ea310d9a5bff57d26eb123474349f91996bf85d
Fabio3rs <fabio3rs@gmail.com>
2015-08-24 00:40:14 -0300

    CGamePlay.h

---
+++ Guitar++/CGamePlay.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef _GUITAR_PP_CGAMEPLAY_h_
+#define _GUITAR_PP_CGAMEPLAY_h_
+
+#include <memory>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <string>
+#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include "CPlayer.h"
+
+class CGamePlay{
+
+public:
+	std::deque < CPlayer > players;
+
+	//static CGamePlay &gamePlay();
+
+	void update();
+	void render();
+
+	CGamePlay();
+};
+
+
+#endif
